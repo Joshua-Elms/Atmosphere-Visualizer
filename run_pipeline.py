47cba6b90cd099b05996228c5b67420ad330d7d6
Joshua Elms <joshua.elms111@gmail.com>
2023-12-03 16:30:13 -0500

    katrina videos

--- run_pipeline.py
+++ run_pipeline.py
@@ -7,21 +7,21 @@ from pipeline import main, setup_logger
 
 # data params
 data_params = dict( 
-    year = "2023",
-    month = "09", # only works within one month of one year (can't download June-July data, for example)
-    start_day_inc = "01",
+    year = "2005",
+    month = "08", # only works within one month of one year (can't download June-July data, for example)
+    start_day_inc = "04",
     stop_day_inc = "30",
     step_day = 1,
     start_hour_inc = "00", # 00-23
     stop_hour_inc = "23", # 00-23
     step_hour = 1,
-    sfc_vars = ["total_column_water_vapour", "2m_temperature", "total_cloud_cover", "vertical_integral_of_total_energy"],
-    pl_vars = [], # "u_component_of_wind", "v_component_of_wind", "geopotential"
-    pl_levels = [] # 500, 1000
+    sfc_vars = [], # "total_column_water_vapour", "total_cloud_cover", "mean_total_precipitation_rate"
+    pl_vars = ["u_component_of_wind", "v_component_of_wind", "geopotential"], # 
+    pl_levels = [500, 850, 1000] # 500, 1000
 )
 
 # Where to save the data, images, and videos. Please provide an absolute path, not a relative path.
-working_dir = pathlib.Path('/Users/joshuaelms/Desktop/github_repos/Atmosphere-Visualizer/large_output')
+working_dir = pathlib.Path('/Users/joshuaelms/Desktop/github_repos/Atmosphere-Visualizer/katrina')
 
 # Output stem for the data
 output_stem = f'y{data_params["year"]}_m{data_params["month"]}_da{data_params["start_day_inc"]}_db{data_params["stop_day_inc"]}_ha{data_params["start_hour_inc"]}_hb{data_params["stop_hour_inc"]}'
@@ -34,7 +34,7 @@ working_dir = working_dir,
 output_stem = output_stem,
 output_stem_explain = r"yYYYY_mMM_diDD_djDD_hmHH_hnHH_{pl,sfc,merged}.nc, where YYYY is the year, MM is the month, DD is the day (a=start, b=stop) inclusive, HH is the hour (a=start, b=stop) inclusive, final tag indicates whether using pl (pressure level data) or sfc (surface data) or merged (combined pl and sfc).",
 output_ds_path = working_dir / f'{output_stem}_merged.nc', # save the output dataset to a netcdf file or False / None to not save
-use_ds = "large_output/y2023_m09_da01_db30_ha00_hb23_merged.nc", # provide a path to a dataset to use instead of pulling data from cdsapi, or False to pull data from cdsapi
+use_ds = "katrina/y2005_m08_da01_db30_ha00_hb23_sfc.nc", # provide a path to a dataset to use instead of pulling data from cdsapi, or False to pull data from cdsapi
 rm_originals = True, # delete the original .nc files after merging and processing
 rm_images = True, # delete the images after creating the video
 img_dir = working_dir / 'frames', # directory to save images to
@@ -47,6 +47,7 @@ channel_metadata = {
     "tcwv": dict(pref_cmap = "ocean"),
     "wind500": dict(pref_cmap = "viridis"),
     "wind1000": dict(pref_cmap = "viridis"),
+    "mtpr": dict(pref_cmap = "Blues"),
 },
 border_color = 'black',
 fps = '18', # frames per second for the video, as a string,
